5c5ae1174f8cf747afd1b7e70316f90f800d9ff6
Cleaverj <78376892+Cleaverj@users.noreply.github.com>
2021-04-20 04:28:27 -0400

    started on billboard beautiful soup

---
+++ Billboard top 100.py
@@ -0,0 +1,8 @@
+#this will use beautiful soup to scrape the billboard top 100 for the most popular songs world wide.
+
+from bs4 import BeautifulSoup
+import requests
+import re
+import os
+import csv
+import unittest
