1ca689621a990720e3d04ed928e56611ffee680a
Cleaverj <78376892+Cleaverj@users.noreply.github.com>
2021-04-20 05:21:46 -0400

    very messy code, will break it up into multiple functions in the morning and get it working from there.

--- Billboard top 100.py
+++ Billboard top 100.py
@@ -15,13 +15,15 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
-def create_database(cur, conn):
-    cur.execute('CREATE TABLE IF NOT EXISTS Billboard (Rank INTEGER PRIMARY KEY, Song TEXT, Artist TEXT, Previous Rank INTEGER, Peak Rank INTEGER, Weeks on Charts INTEGER)')
+def create_table(cur, conn):
+    cur.execute('CREATE TABLE IF NOT EXISTS Billboard (Rank INTEGER PRIMARY KEY, Song TEXT, Artist TEXT, Previous_Rank INTEGER, Peak_Rank INTEGER, Weeks_on_Charts INTEGER)')
     conn.commit()
     
 
 def billboard_soup(year, month, day):
     cur, conn = setUpDatabase('GAS_MEDIA.db')  
+
+    create_table(cur, conn)
 
     partial_url = 'https://www.billboard.com/charts/hot-100/'
     updated_url = partial_url + str(year) + '-' + str(month) + '-' + str(day)
@@ -40,15 +42,15 @@ def billboard_soup(year, month, day):
             previous_rank = item.find('span', class_ = 'chart-element__meta text--center color--secondary text--last')
             peak_rank = item.find('span', class_ = 'chart-element__meta text--center color--secondary text--peak')
             wks_on_chart = item.find('span', class_ = 'chart-element__meta text--center color--secondary text--week')
-            
-            cur.execute(f"SELECT song FROM Billboard")
+            print([rank, song, artist, previous_rank, peak_rank, wks_on_chart])
+            cur.execute(f"SELECT Song FROM Billboard")
             rows = cur.fetchall()
-            if song in rows:
+            if song not in rows:
                 cur.execute("INSERT INTO Billboard (Rank, Song, Artist, Previous Rank, Peak Rank, Weeks on Charts) VALUES (?,?,?,?,?,?)",(rank,song,artist,previous_rank, peak_rank,wks_on_chart))
                 conn.commit()
                 i += 1
             else:
                 continue
 
-billboard_soup('2021', '04', '17')
+billboard_soup('2021', '04', '10')
 
