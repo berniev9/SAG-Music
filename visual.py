5819e2a8d6cf735542378515a1650cfb94af3f1d
Bernie Allen Velasquez <berniev@umich.edu>
2021-04-22 11:16:19 -0400

    added file for the visualizations

---
+++ visual.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import os
+import csv
+import sqlite3
+import json
+import plotly.graph_objects as go
+import plotly.express as px
+import matplotlib.pyplot as plt
