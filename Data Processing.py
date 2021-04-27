570e6b694ed34f72977aed02a3909fd837ca528d
Cleaverj <78376892+Cleaverj@users.noreply.github.com>
2021-04-27 00:38:18 -0400

    simple averages calculated for data processed.
    We could do further calculations if you guys dont feel this goes far enough

--- Data Processing.py
+++ Data Processing.py
@@ -14,12 +14,31 @@ def setUpDatabase(db_name):
 def dataprocess(cur, conn):
     cur.execute('SELECT Billboard.Song, Billboard.Rank, Spotify.Popularity, Shazam_Data.Rank_of_song FROM Billboard JOIN Spotify on Billboard.Song = Spotify.Song JOIN Shazam_Data on BillBoard.Song = Shazam_Data.Song')
     rows = cur.fetchall()
-    print(len(rows))
+    ranking_list = []
     for item in rows:
-        print(item)
+        song = item[0]
+        billboard = item[1]
+        spotify = 101 - item[2] # this makes an assumption that popularity from Spotify can be equated to the ranking of the other api/website. this relationship in reality is unknown but it does provide insight into the verying degrees of "popularity" of the songs across platforms
+        shazam = item[3]
+        avg = (billboard + spotify + shazam)/3
+        tup = (song, int(avg)) #use int to round down to nearest whole number.
+        ranking_list.append(tup)
+    ranking_list.sort(key = lambda x: x[1])
+    return ranking_list
+
+def writetotext(cur, conn):
+    data = dataprocess(cur, conn)
+    with open('ProcessedData.txt', 'w') as f:
+        f.write('Below is the average "popularity" out of 100 total of a song between Spotify, Billboard, and Shazam')
+        for item in data:
+            f.write('\n')
+            f.write(f"{item[0]} has a mean popularity of {item[1]}")
+        
+            
+        
 
 def main():
     cur, conn = setUpDatabase('GAS_MEDIA.db')
-    dataprocess(cur, conn)
+    writetotext(cur, conn)
 if __name__ == "__main__":
     main()
